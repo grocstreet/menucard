df174daa29414bee880e1d207bf42a93e86df951
Joe Lewis <lewis.joe.18@gmail.com>
2014-11-16 23:30:39 +0530

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Digital menu card for mobiles and tablets. 
+
+Things to remember: 
+
++ Initially, Use native db engine for storing-retrieving offline data
++ Abstract away data storage and retreival. We may use native db engine or a remote service, say firebase later. 
++ Adapter for cases with/without Internet connectivity. 
++ Interfacing angularjs models with our custom data storage/retreival api. 
+
+
---
+++ www/js/junk.js
@@ -0,0 +1,20 @@
+
+    // .state('app.playlists', {
+    //   url: "/playlists",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/playlists.html",
+    //       controller: 'PlaylistsCtrl'
+    //     }
+    //   }
+    // })
+
+    // .state('app.single', {
+    //   url: "/playlists/:playlistId",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/playlist.html",
+    //       controller: 'PlaylistCtrl'
+    //     }
+    //   }
+    // })
